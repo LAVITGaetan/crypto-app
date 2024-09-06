948866ef6245f57368ca49b82cd00ccae68906b0
LAVITGaetan <g.lavit@outlook.fr>
2024-09-06 16:30:42 +0400

    add basic UI with responsive for landing page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeroComponent } from './elements/hero/hero.component';
+import { CryptoListComponent } from './elements/crypto-list/crypto-list.component';
+import { NavbarComponent } from './elements/navbar/navbar.component';
+import { FooterComponent } from './elements/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroComponent,
+    CryptoListComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
